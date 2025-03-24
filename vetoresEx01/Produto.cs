9eee4c20db8c69d0761380dbf2f84429c0049b3e
Rubenslourenco <alvesrubens926@gmail.com>
2025-03-24 15:04:36 -0300

    feat: criado novo projeto é atualizando as pasta

---
+++ vetoresEx01/Produto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*************  ✨ Codeium Command 🌟  *************/
+    public class Produto
+
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco) {
+            Nome = nome;
+            Preco = preco;
+        }
+     
+    
+/******  5a47dbfd-6bb5-4c76-bf9d-2ec22814b2c6  *******/}
---
+++ vetoresEx01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()??"0");
+
+            Produto[] vect =  new Produto[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string nome = Console.ReadLine() ?? "";
+                double preco = double.Parse(Console.ReadLine() ?? "0", CultureInfo.InvariantCulture);
+                vect[i] = new Produto(nome = nome,preco = preco);
+                
+            }
+
+            double sum = 0.0;
+            for  (int i = 0; i <n; i++) {
+                sum += vect[i].preco;
+            }
+
+            double avg = sum / n;
+
+
+            Console.WriteLine("Average price: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        
+        }
