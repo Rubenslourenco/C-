2981fec1b65e29dfbe0403e937d8c87e1e8debd4
Rubenslourenco <alvesrubens926@gmail.com>
2025-04-02 15:08:56 -0300

    Feat: upcasting e downcasting

--- ProjetoAccount01/Program.cs
+++ ProjetoAccount01/Program.cs
@@ -7,9 +7,33 @@ namespace ProjetoAccount01
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010, "Maria", 100.0, 500.0);
+            // BusinessAccount account = new BusinessAccount(8010, "Maria", 100.0, 500.0);
 
-            System.Console.WriteLine(account.Balance);
+            // System.Console.WriteLine(account.Balance);
+
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            // Upcast
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+            ///Downcasting
+
+            if (acc3 is BusinessAccount)
+            {
+                BusinessAccount acc4 = (BusinessAccount)acc3;
+                acc4.Loan(200.0);
+                System.Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpdateBalance();
+                System.Console.WriteLine("Update!");
+            }
 
 
         }
